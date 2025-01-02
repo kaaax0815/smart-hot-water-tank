72025c19963fd06edb7560f9a55f692e0b7e6138
Bernd Storath <999999bst@gmail.com>
2025-01-02 23:48:15 +0100

    add wifi, better fonts

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,45 @@
 use std::{
     sync::{
         atomic::{AtomicI32, Ordering},
-        Arc,
+        Arc, LazyLock, RwLock,
     },
     thread,
 };
 
-use embedded_graphics::{
-    mono_font::{ascii::FONT_10X20, MonoTextStyle},
-    prelude::*,
-    text::Text,
-};
+use embedded_graphics::prelude::*;
 
 use epd_waveshare::{epd2in9_v2::*, prelude::*};
 
-use esp_idf_svc::hal::{
-    delay::Delay,
-    gpio::{self, PinDriver},
-    peripherals,
-    spi::{self, config::DriverConfig, SpiDeviceDriver, SpiDriver},
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::{
+        delay::Delay,
+        gpio::{self, PinDriver},
+        peripherals,
+        spi::{self, config::DriverConfig, SpiDeviceDriver, SpiDriver},
+    },
+    nvs::EspDefaultNvsPartition,
+    wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
 
 use max31855_rs::Max31855;
+use u8g2_fonts::{
+    fonts,
+    types::{HorizontalAlignment, VerticalPosition},
+    FontRenderer,
+};
 
-#[allow(dead_code)]
-const HEIGHT: u32 = 128;
-#[allow(dead_code)]
-const WIDTH: u32 = 296;
+const SSID: &str = env!("WIFI_SSID");
+const PASSWORD: &str = env!("WIFI_PASS");
 
 static TEMP: AtomicI32 = AtomicI32::new(-25000);
 
-fn main() {
+static IP: LazyLock<Arc<RwLock<String>>> =
+    LazyLock::new(|| Arc::new(RwLock::new("Not available".to_string())));
+
+// TODO: avoid unwrap, handle gracefully
+
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -38,30 +47,47 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello from esp32");
+    log::info!("main: Starting smart-hot-water-tank");
 
-    let peripherals = peripherals::Peripherals::take().unwrap();
+    let peripherals = peripherals::Peripherals::take()?;
 
+    // display
+    let rst = PinDriver::output(peripherals.pins.gpio10)?;
+    let dc = PinDriver::output(peripherals.pins.gpio11)?;
+    let busy = PinDriver::input(peripherals.pins.gpio22)?;
+
+    // spi
     let spi = peripherals.spi2;
-
-    let rst = PinDriver::output(peripherals.pins.gpio10).unwrap();
-    let dc = PinDriver::output(peripherals.pins.gpio11).unwrap();
-    let busy = PinDriver::input(peripherals.pins.gpio22).unwrap();
-
     let sclk = peripherals.pins.gpio6;
     let mosi = peripherals.pins.gpio7;
     let miso = peripherals.pins.gpio2;
+    // display
     let cs0 = peripherals.pins.gpio19;
+    // amplifier
     let cs1 = peripherals.pins.gpio18;
 
     let config = spi::config::Config::default();
 
-    // doesn't work properly
-    let spi_driver =
-        Arc::new(SpiDriver::new(spi, sclk, mosi, Some(miso), &DriverConfig::default()).unwrap());
+    let spi_driver = Arc::new(SpiDriver::new(
+        spi,
+        sclk,
+        mosi,
+        Some(miso),
+        &DriverConfig::default(),
+    )?);
 
-    let spi0 = SpiDeviceDriver::new(spi_driver.clone(), Some(cs0), &config).unwrap();
-    let spi1 = SpiDeviceDriver::new(spi_driver.clone(), Some(cs1), &config).unwrap();
+    // display
+    let spi0 = SpiDeviceDriver::new(spi_driver.clone(), Some(cs0), &config)?;
+    // amplifier
+    let spi1 = SpiDeviceDriver::new(spi_driver.clone(), Some(cs1), &config)?;
+
+    // wifi
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+    let wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop,
+    )?;
 
     thread::Builder::new()
         .name("display".to_string())
@@ -69,11 +95,10 @@ fn main() {
         .spawn(|| {
             let display_res = display(spi0, busy, dc, rst);
             match display_res {
-                Ok(_) => log::info!("Display thread exited successfully"),
-                Err(_) => log::error!("Display thread exited with an error"),
+                Ok(_) => log::info!("main: Display thread exited successfully"),
+                Err(e) => log::error!("main: Display thread exited with an error. {:?}", e),
             }
-        })
-        .unwrap();
+        })?;
 
     thread::Builder::new()
         .name("sensor".to_string())
@@ -81,11 +106,21 @@ fn main() {
         .spawn(|| {
             let sensor_res = sensor(spi1);
             match sensor_res {
-                Ok(_) => log::info!("Sensor thread exited successfully"),
-                Err(_) => log::error!("Sensor thread exited with an error"),
+                Ok(_) => log::info!("main: Sensor thread exited successfully"),
+                Err(e) => log::error!("main: Sensor thread exited with an error. {:?}", e),
             }
-        })
-        .unwrap();
+        })?;
+
+    thread::Builder::new()
+        .name("network".to_string())
+        .stack_size(8192)
+        .spawn(|| {
+            let sensor_res = network(wifi);
+            match sensor_res {
+                Ok(_) => log::info!("main: Network thread exited successfully"),
+                Err(e) => log::error!("main: Network thread exited with an error. {:?}", e),
+            }
+        })?;
 
     loop {
         thread::park();
@@ -97,12 +132,16 @@ fn display<'a>(
     busy: PinDriver<'a, gpio::Gpio22, gpio::Input>,
     dc: PinDriver<'a, gpio::Gpio11, gpio::Output>,
     rst: PinDriver<'a, gpio::Gpio10, gpio::Output>,
-) -> Result<(), &'a str> {
+) -> anyhow::Result<()> {
     let mut delay = Delay::new_default();
 
     let mut epd = Epd2in9::new(&mut spi0, busy, dc, rst, &mut delay, None).unwrap();
     let mut display = Display2in9::default();
     display.set_rotation(DisplayRotation::Rotate90);
+    display.clear(Color::White).unwrap();
+
+    epd.update_and_display_frame(&mut spi0, display.buffer(), &mut delay)
+        .unwrap();
 
     epd.update_old_frame(&mut spi0, display.buffer(), &mut delay)
         .unwrap();
@@ -112,16 +151,34 @@ fn display<'a>(
 
         display.clear(Color::White).unwrap();
 
-        let temp = TEMP.load(Ordering::SeqCst) as f32 / 100_f32;
+        // ip
+        {
+            let ip = IP.try_read();
+            match ip {
+                Ok(ip) => {
+                    write_ip(&mut display, ip.to_string()).unwrap();
+                }
+                Err(_) => {
+                    log::error!("display: IP: Lock not available");
+                }
+            }
+        }
+        // temp
+        {
+            let temp = TEMP.load(Ordering::SeqCst) as f32 / 100_f32;
 
-        write_text(&mut display, format!("{}", temp).as_str(), 10, 30).unwrap();
+            let integer_part = temp.trunc() as i32;
+            let fractional_part = (temp.fract() * 100_f32) as i32;
+
+            write_temp(&mut display, integer_part, fractional_part).unwrap();
+        }
 
         epd.update_and_display_new_frame(&mut spi0, display.buffer(), &mut delay)
             .unwrap();
     }
 }
 
-fn sensor<'a>(spi1: SpiDeviceDriver<'a, Arc<SpiDriver<'a>>>) -> Result<(), &'a str> {
+fn sensor<'a>(spi1: SpiDeviceDriver<'a, Arc<SpiDriver<'a>>>) -> anyhow::Result<()> {
     let mut max = Max31855::new(spi1);
 
     let delay = Delay::new_default();
@@ -135,21 +192,84 @@ fn sensor<'a>(spi1: SpiDeviceDriver<'a, Arc<SpiDriver<'a>>>) -> Result<(), &'a s
 
         TEMP.store(thermo_i, Ordering::SeqCst);
 
-        log::info!("Thermo: {}°C", thermo_c);
+        log::info!("sensor: Thermo: {}°C", thermo_c);
     }
 }
 
-fn write_text<'a>(
-    display: &'a mut Display2in9,
-    text: &'a str,
-    x: i32,
-    y: i32,
-) -> Result<(), &'a str> {
-    let style = MonoTextStyle::new(&FONT_10X20, Color::Black);
+fn network<'a>(mut wifi: BlockingWifi<EspWifi<'a>>) -> anyhow::Result<()> {
+    let delay = Delay::new_default();
 
-    Text::new(text, Point::new(x, y), style)
-        .draw(display)
-        .unwrap();
+    // TODO: gracefully handle timeouts or disconnects
+    connect_wifi(&mut wifi)?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+
+    log::info!("network : Wifi connected with IP: {:?}", ip_info);
+
+    loop {
+        delay.delay_ms(1000);
+        let mut write_lock = IP.write().unwrap();
+        let ip = wifi.wifi().sta_netif().get_ip_info()?;
+        *write_lock = ip.ip.to_string();
+        drop(write_lock);
+    }
+}
+
+fn write_temp(display: &mut Display2in9, int: i32, frac: i32) -> anyhow::Result<()> {
+    let font = FontRenderer::new::<fonts::u8g2_font_logisoso58_tf>();
+
+    // render temp, int and frac part. int part should be larger then the frac part
+    let text = &*format!("{}.{:02}°C", int, frac);
+
+    font.render_aligned(
+        text,
+        Point::new(280, 64),
+        VerticalPosition::Center,
+        HorizontalAlignment::Right,
+        u8g2_fonts::types::FontColor::Transparent(Color::Black),
+        display,
+    )
+    .unwrap();
+
+    Ok(())
+}
+
+fn connect_wifi<'a>(wifi: &mut BlockingWifi<EspWifi<'a>>) -> anyhow::Result<()> {
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2WPA3Personal,
+        password: PASSWORD.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start()?;
+    log::info!("wifi: Wifi started");
+
+    wifi.connect()?;
+    log::info!("wifi: Wifi connected");
+
+    wifi.wait_netif_up()?;
+    log::info!("wifi: Wifi netif up");
+
+    Ok(())
+}
+
+fn write_ip(display: &mut Display2in9, ip: String) -> anyhow::Result<()> {
+    let font = FontRenderer::new::<fonts::u8g2_font_logisoso16_tr>();
+
+    font.render_aligned(
+        &*ip,
+        Point::new(0, 128),
+        VerticalPosition::Bottom,
+        HorizontalAlignment::Left,
+        u8g2_fonts::types::FontColor::Transparent(Color::Black),
+        display,
+    )
+    .unwrap();
 
     Ok(())
 }
